3a1e8aa5c3fa8604ac38e9821ae3242cf7a230e8
gray_jiang <Gray_Jy@yeah.net>
2023-11-23 17:20:12 +0800

    🦄 refactor: add the env

---
+++ script/DeployDSC.s.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import {Script} from "forge-std/Script.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
+
+contract DeployDSC is Script {
+    // HelperConfig public helperConfig;
+
+    constructor() {}
+
+    function run()
+        external
+        returns (
+            /**
+             * (Contract contract)
+             */
+            HelperConfig helperConfig
+        )
+    {
+        helperConfig = new HelperConfig();
+
+        vm.startBroadcast();
+        // deploy your contract here...
+        vm.stopBroadcast();
+    }
+}
---
+++ script/HelperConfig.s.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+contract HelperConfig {
+    struct NetworkConfig {
+        uint256 name;
+    }
+
+    uint256 public DEFAULT_ANVIL_PRIVATE_KEY = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
+
+    constructor() {}
+}
