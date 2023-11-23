755b035e1a562260e2113a7bb1bee10a65feb9d7
gray_jiang <Gray_Jy@yeah.net>
2023-11-23 23:13:35 +0800

    ✨ feat(DeployDSC): deploy the token and engine

--- script/DeployDSC.s.sol
+++ script/DeployDSC.s.sol
@@ -4,14 +4,20 @@ pragma solidity ^0.8.18;
 import {Script, console} from "forge-std/Script.sol";
 import {HelperConfig} from "./HelperConfig.s.sol";
 import {DecentralizedStableCoin} from "../src/DecentralizedStableCoin.sol";
+import {DSCEngine} from "../src/DSCEngine.sol";
 
 contract DeployDSC is Script {
+    address[] public tokenAddresses;
+    address[] public priceFeedAddresses;
+
     constructor() {}
 
-    function run() external returns (DecentralizedStableCoin dsc, HelperConfig helperConfig) {
+    function run() external returns (DecentralizedStableCoin dsc, DSCEngine dscEngine, HelperConfig helperConfig) {
         helperConfig = new HelperConfig();
         (address wethUsdPriceFeed, address wbtcUsdPriceFeed, address weth, address wbtc, uint256 deployerKey) =
             helperConfig.activeNetworkConfig();
+        tokenAddresses = [weth, wbtc];
+        priceFeedAddresses = [wethUsdPriceFeed, wbtcUsdPriceFeed];
         console.log("wethUsdPriceFeed: %s", wethUsdPriceFeed);
         console.log("wbtcUsdPriceFeed: %s", wbtcUsdPriceFeed);
         console.log("weth: %s", weth);
@@ -19,7 +25,10 @@ contract DeployDSC is Script {
         console.log("deployerKey: %s", deployerKey);
         vm.startBroadcast(deployerKey);
         dsc = new DecentralizedStableCoin();
+        dscEngine = new DSCEngine(tokenAddresses,priceFeedAddresses,address(dsc));
+        dsc.transferOwnership(address(dscEngine)); // cuz the dsc is Ownable, so we need to transfer the ownership to the dscEngine
         vm.stopBroadcast();
         console.log("dsc: %s", address(dsc));
+        console.log("dscEngine: %s", address(dscEngine));
     }
 }
